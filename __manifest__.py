37ba1b375a3c6fbaa5905cb59cf1df81abda5784
Ajay <ajay@Ajays-MacBook-Air.local>
2022-09-19 14:24:19 +0530

    security file updated

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.3',
+    'version': '1.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base','crm', 'base_vat', 'jobsites'],
