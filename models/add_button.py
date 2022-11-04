8e66cd379df1a71f0fd5e1a3e883e1b5fd1ca418
ajay <ajjayymahato@gmail.com>
2022-11-04 11:19:15 +0530

    uncomment LQ working/AR AM BDE auto assign

--- models/add_button.py
+++ models/add_button.py
@@ -15,14 +15,14 @@ class CrmLead(models.Model):
 
     in_beta = fields.Boolean(default=False, string="Add Customer To Beta")
 
-    # def write(self, vals):
-    #     if self.team_id.user_id.id == self._uid:
-    #         return super(CrmLead, self).write(vals)
-    #
-    #     if self.lead_qual:
-    #         if (self._uid != self.user_id.id) and self.type == 'lead':
-    #             raise ValidationError(_('You cant change Salesperson unless you are assigned to it!'))
-    #         elif (self.lead_qual.lower() not in self.user_id.name.lower()) and self.type == 'lead':
-    #             raise ValidationError(_('You cant change Salesperson unless you are assigned as LQ to it!'))
-    #     return super(CrmLead, self).write(vals)
+    def write(self, vals):
+        if self.team_id.user_id.id == self._uid:
+            return super(CrmLead, self).write(vals)
+
+        if self.lead_qual:
+            if (self._uid != self.user_id.id) and self.type == 'lead':
+                raise ValidationError(_('You cant change Salesperson unless you are assigned to it!'))
+            elif (self.lead_qual.lower() not in self.user_id.name.lower()) and self.type == 'lead':
+                raise ValidationError(_('You cant change Salesperson unless you are assigned as LQ to it!'))
+        return super(CrmLead, self).write(vals)
 
